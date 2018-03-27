7270a9a987946abda95430393f5c755bcd2e048c
juan <theaxford@gmail.com>
2018-03-27 18:01:35 +0200

    Minor changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => res.render('index', { title: 'Express' }));
+router.get('/', (req, res) => res.render('index', { title: 'Express' }));
 
 module.exports = router;
