12dbbbdcf995a26ab47960accdb0d3b0dc5a4067
Axford <thaxford@gmail.com>
2018-04-25 17:58:11 +0200

    clase 5

--- server/models/locations.js
+++ server/models/locations.js
@@ -1,51 +1,51 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
   author: String,
   rating: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 5
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
   },
   reviewText: String,
   createdOn: {
-      type: Date,
-      "default": Date.now
+    type: Date,
+    "default": Date.now
   }
 });
 
 const openingTimeSchema = new Schema({
   days: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   opening: String,
   closing: String,
   closed: {
-      type: Boolean,
-      required: true
+    type: Boolean,
+    required: true
   }
 });
 
 const locationSchema = new Schema({
   name: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   address: String,
   rating: {
-      type: Number,
-      "default": 0,
-      min: 0,
-      max: 5
+    type: Number,
+    "default": 0,
+    min: 0,
+    max: 5
   },
   facilities: [String],
   // Always store coordinates longitude, latitude order.
   coords: {
-      type: [Number],
-      index: '2dsphere'
+    type: [Number],
+    index: '2dsphere'
   },
   openingTimes: [openingTimeSchema],
   reviews: [reviewSchema]
